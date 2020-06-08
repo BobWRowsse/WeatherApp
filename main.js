6b37d5a577d13af17f08f74350be1b5c068ab40c
BobWRowsse <bobrowsse.walakira@hyperisland.se>
2020-06-08 20:54:49 +0200

    add dateBuilder

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ searchbox.addEventListener('keypress', setQuery);
 function setQuery(evt) {
   if (evt.keyCode == 13) {
     getResults(searchbox.value);
-    console.log(searchbox.value);
   }
 }
 
@@ -23,4 +22,22 @@ function getResults(query) {
 
 function displayResults(weather) {
   console.log(weather);
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+}
+
+function dateBuilder(d) {
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
 }
