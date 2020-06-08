119caf1386d5857000664936c1da7407dac5a08f
BobWRowsse <bobrowsse.walakira@hyperisland.se>
2020-06-08 20:31:50 +0200

    add API key to main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const api = {
+  key: '92aaa7f63c6be6f2f42f9165803964df',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+    console.log(searchbox.value);
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`);
+}
